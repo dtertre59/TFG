98fcd861b4de95938a7403165fae51b53d94b8c8
dtertre59 <davidtertreboye@gmail.com>
2024-05-17 09:58:47 +0200

    yolo pose estimation v1

--- pruebas.py
+++ pruebas.py
@@ -1,26 +1,30 @@
-# from ultralytics import YOLO
+from ultralytics import YOLO
 
-# #Load a model
-# # model = YOLO('yolov8n-pose.pt')  # load an official model
-# model = YOLO(fr'tigre_model_1.pt')
-# # model = YOLO(fr'square_model_1.pt')
+#Load a model
+# model = YOLO('yolov8n-pose.pt')  # load an official model
+model = YOLO('/home/robotica/Documents/dtertre59/TFG/app/assets/nn_models/yolov8n_poseEstimation_square_v1.pt')
+# model = YOLO(fr'square_model_1.pt')
 
-# # Predict with the model
-# results = model('tigre2.png')  # predict on an image
+# Predict with the model
+results = model('/home/robotica/Documents/dtertre59/TFG/app/assets/pictures/train/square/square_12.png')  # predict on an image
 
-# # View results
-# for r in results:
-#     print(r.keypoints)  # print the Keypoints object containing the detected keypoints
-#     r.show()
+print(results)
 
-def ejemplo(*args, **kwargs):
-    print("Argumentos posicionales:")
-    for arg in args:
-        print(arg)
+# View results
+for r in results:
+    print(r.keypoints)  # print the Keypoints object containing the detected keypoints
+    r.show()
+
+    
+
+# def ejemplo(*args, **kwargs):
+#     print("Argumentos posicionales:")
+#     for arg in args:
+#         print(arg)
         
-    print("\nArgumentos de palabra clave:")
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
+#     print("\nArgumentos de palabra clave:")
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}")
 
-# Llamada a la función ejemplo con diferentes tipos de argumentos
-ejemplo(1, 2, 3, nombre="Alice", edad=30)
+# # Llamada a la función ejemplo con diferentes tipos de argumentos
+# ejemplo(1, 2, 3, nombre="Alice", edad=30)
